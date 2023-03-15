7e0d819140b7de8395ad6529692d631ead38d62d
marko.b <adriadrop@gmail.com>
2023-03-15 12:10:47 +0100

    update

---
+++ _next/static/rjChEBmZ7O93eBRieciqA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/909-f6ff547d0a9b2867.js","static/chunks/pages/index-b56add0e9ada4b69.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
