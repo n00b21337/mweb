3d8e9c4141c42eec0c81e11ce7077ee3ed1aa107
marko.b <adriadrop@gmail.com>
2023-03-22 17:53:00 +0100

    update with images, css, fonts and all

---
+++ _next/static/G3kOoRnCehZwHttYTqZlV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/909-70cefd6c9258295d.js","static/chunks/pages/index-61677b91e74a66d7.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
