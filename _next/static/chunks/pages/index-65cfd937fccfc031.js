61ecd29bdf1b355848a6dc78cd89a596259c337b
marko.b <adriadrop@gmail.com>
2023-05-29 21:27:45 +0200

    change description

--- _next/static/chunks/pages/index-65cfd937fccfc031.js
+++ _next/static/chunks/pages/index-65cfd937fccfc031.js
@@ -1,1 +1,176 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{75557:function(e,s,a){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return a(69190)}])},69190:function(e,s,a){"use strict";a.r(s),a.d(s,{default:function(){return g}});var i=a(85893),n=a(9008),t=a.n(n),r=a(73193),m=a(37673),c=(a(67294),a(25675),a(70594)),l=a(46563),o=(a(26973),a(84941),a(55767));function d(){return(0,i.jsxs)("div",{className:"main",children:[(0,i.jsxs)("div",{className:"main__section",children:[(0,i.jsx)("div",{className:"main__hero-bg",children:(0,i.jsx)("div",{className:"hero-bg__wrapper",children:(0,i.jsx)("div",{className:"decor__floor"})})}),(0,i.jsx)("div",{className:"container",children:(0,i.jsxs)("div",{className:"main__content",children:[(0,i.jsx)("div",{className:"title-strokes w-embed"}),(0,i.jsx)("div",{className:"main__title_mini lower-fs",children:"The"}),(0,i.jsx)("h1",{className:"main__title",children:"Managers"}),(0,i.jsxs)("div",{className:"main__title-decor-wrapper",children:[(0,i.jsx)("img",{src:"images/green-pin-new.png",loading:"lazy",alt:"pin",className:"main__title-decor1"}),(0,i.jsx)("img",{src:"images/red-pin-new.png",loading:"lazy",alt:"pin",className:"main__title-decor2"}),(0,i.jsxs)("div",{className:"decor__mans-wrapper",children:[(0,i.jsx)("img",{src:"images/left-side-mans.png",loading:"lazy",alt:"managers",className:"left-side-mans"}),(0,i.jsx)("img",{src:"images/right-side-mans.png",loading:"lazy",sizes:"(max-width: 767px) 100vw, 527px",srcSet:"images/right-side-mans-p-500.png 500w, images/right-side-mans.png 527w",alt:"managers_r",className:"right-side-mans"}),(0,i.jsx)("img",{src:"images/mans_mob.png",loading:"lazy",sizes:"(max-width: 479px) 100vw, (max-width: 767px) 528px, 100vw",srcSet:"images/mans_mob-p-500.png 500w, images/mans_mob-p-800.png 800w, images/mans_mob.png 896w",alt:"managers_m",className:"decor-mans-mob"})]})]}),(0,i.jsx)("p",{className:"main__description",children:"Experience the new ERC6551 NFT standard, and join the new era in ownership of digital assets. The world needs bold decision-makers. Are you one of them?"}),(0,i.jsx)(o.Z,{})]})})]}),(0,i.jsx)(c.Z,{}),(0,i.jsx)(l.Z,{})]})}function g(){return(0,i.jsxs)("div",{className:"body",children:[(0,i.jsxs)(t(),{children:[(0,i.jsx)("meta",{charSet:"utf-8"}),(0,i.jsx)("title",{children:"The Managers"}),(0,i.jsx)("meta",{content:"width=device-width, initial-scale=1",name:"viewport"}),(0,i.jsx)("link",{href:"images/favicon.ico",rel:"shortcut icon",type:"image/x-icon"}),(0,i.jsx)("link",{href:"images/webclip.png",rel:"apple-touch-icon"}),(0,i.jsx)("meta",{name:"description",content:"It's about the right time for Managers to take over; enough with your soft emotional baloney. The world needs bold decision-makers. Are you one of them?"}),(0,i.jsx)("meta",{property:"og:image",content:"https://themanagers.wtf/images/cover.png"}),(0,i.jsx)("meta",{name:"twitter:image",content:"https://themanagers.wtf/images/cover.png"})]}),(0,i.jsxs)("div",{className:"page_wrapper",children:[(0,i.jsx)(m.Z,{}),(0,i.jsx)(d,{}),(0,i.jsx)(r.Z,{})]})]})}}},function(e){e.O(0,[557,296,774,888,179],(function(){return s=75557,e(e.s=s);var s}));var s=e.O();_N_E=s}]);
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [405],
+  {
+    75557: function (e, s, a) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        "/",
+        function () {
+          return a(69190);
+        },
+      ]);
+    },
+    69190: function (e, s, a) {
+      "use strict";
+      a.r(s),
+        a.d(s, {
+          default: function () {
+            return g;
+          },
+        });
+      var i = a(85893),
+        n = a(9008),
+        t = a.n(n),
+        r = a(73193),
+        m = a(37673),
+        c = (a(67294), a(25675), a(70594)),
+        l = a(46563),
+        o = (a(26973), a(84941), a(55767));
+      function d() {
+        return (0, i.jsxs)("div", {
+          className: "main",
+          children: [
+            (0, i.jsxs)("div", {
+              className: "main__section",
+              children: [
+                (0, i.jsx)("div", {
+                  className: "main__hero-bg",
+                  children: (0, i.jsx)("div", {
+                    className: "hero-bg__wrapper",
+                    children: (0, i.jsx)("div", { className: "decor__floor" }),
+                  }),
+                }),
+                (0, i.jsx)("div", {
+                  className: "container",
+                  children: (0, i.jsxs)("div", {
+                    className: "main__content",
+                    children: [
+                      (0, i.jsx)("div", { className: "title-strokes w-embed" }),
+                      (0, i.jsx)("div", {
+                        className: "main__title_mini lower-fs",
+                        children: "The",
+                      }),
+                      (0, i.jsx)("h1", {
+                        className: "main__title",
+                        children: "Managers",
+                      }),
+                      (0, i.jsxs)("div", {
+                        className: "main__title-decor-wrapper",
+                        children: [
+                          (0, i.jsx)("img", {
+                            src: "images/green-pin-new.png",
+                            loading: "lazy",
+                            alt: "pin",
+                            className: "main__title-decor1",
+                          }),
+                          (0, i.jsx)("img", {
+                            src: "images/red-pin-new.png",
+                            loading: "lazy",
+                            alt: "pin",
+                            className: "main__title-decor2",
+                          }),
+                          (0, i.jsxs)("div", {
+                            className: "decor__mans-wrapper",
+                            children: [
+                              (0, i.jsx)("img", {
+                                src: "images/left-side-mans.png",
+                                loading: "lazy",
+                                alt: "managers",
+                                className: "left-side-mans",
+                              }),
+                              (0, i.jsx)("img", {
+                                src: "images/right-side-mans.png",
+                                loading: "lazy",
+                                sizes: "(max-width: 767px) 100vw, 527px",
+                                srcSet:
+                                  "images/right-side-mans-p-500.png 500w, images/right-side-mans.png 527w",
+                                alt: "managers_r",
+                                className: "right-side-mans",
+                              }),
+                              (0, i.jsx)("img", {
+                                src: "images/mans_mob.png",
+                                loading: "lazy",
+                                sizes:
+                                  "(max-width: 479px) 100vw, (max-width: 767px) 528px, 100vw",
+                                srcSet:
+                                  "images/mans_mob-p-500.png 500w, images/mans_mob-p-800.png 800w, images/mans_mob.png 896w",
+                                alt: "managers_m",
+                                className: "decor-mans-mob",
+                              }),
+                            ],
+                          }),
+                        ],
+                      }),
+                      (0, i.jsx)("p", {
+                        className: "main__description",
+                        children:
+                          "Experience the new ERC6551 NFT standard, and join the new era in ownership of digital assets. The world needs bold decision-makers. Are you one of them?",
+                      }),
+                      (0, i.jsx)(o.Z, {}),
+                    ],
+                  }),
+                }),
+              ],
+            }),
+            (0, i.jsx)(c.Z, {}),
+            (0, i.jsx)(l.Z, {}),
+          ],
+        });
+      }
+      function g() {
+        return (0, i.jsxs)("div", {
+          className: "body",
+          children: [
+            (0, i.jsxs)(t(), {
+              children: [
+                (0, i.jsx)("meta", { charSet: "utf-8" }),
+                (0, i.jsx)("title", { children: "The Managers" }),
+                (0, i.jsx)("meta", {
+                  content: "width=device-width, initial-scale=1",
+                  name: "viewport",
+                }),
+                (0, i.jsx)("link", {
+                  href: "images/favicon.ico",
+                  rel: "shortcut icon",
+                  type: "image/x-icon",
+                }),
+                (0, i.jsx)("link", {
+                  href: "images/webclip.png",
+                  rel: "apple-touch-icon",
+                }),
+                (0, i.jsx)("meta", {
+                  name: "description",
+                  content:
+                    "Experience the new ERC6551 NFT standard, and join the new era in ownership of digital assets. The world needs bold decision-makers. Are you one of them?",
+                }),
+                (0, i.jsx)("meta", {
+                  property: "og:image",
+                  content: "https://themanagers.wtf/images/cover.png",
+                }),
+                (0, i.jsx)("meta", {
+                  name: "twitter:image",
+                  content: "https://themanagers.wtf/images/cover.png",
+                }),
+              ],
+            }),
+            (0, i.jsxs)("div", {
+              className: "page_wrapper",
+              children: [
+                (0, i.jsx)(m.Z, {}),
+                (0, i.jsx)(d, {}),
+                (0, i.jsx)(r.Z, {}),
+              ],
+            }),
+          ],
+        });
+      }
+    },
+  },
+  function (e) {
+    e.O(0, [557, 296, 774, 888, 179], function () {
+      return (s = 75557), e((e.s = s));
+      var s;
+    });
+    var s = e.O();
+    _N_E = s;
+  },
+]);
